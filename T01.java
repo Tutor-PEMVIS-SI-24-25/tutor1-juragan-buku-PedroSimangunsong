6735fdab132e2d5f7dd5567298ba8e86dcd2f99e
githubPedroSimangunsong <SimangunsongPedro2@gmail.com>
2024-09-04 21:28:31 +0700

    Update T01.java

--- T01.java
+++ T01.java
@@ -1,3 +1,16 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24011 - Pedro Simangunsong
+// 12S24011 - Pedro Simangunsong
 
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String pedro;
+
+        pedro = input.nextLine();
+        System.out.println("Nama");
+    }
+}
